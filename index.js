073b40bfa9270c1e8aacdcc5291f14389e8b2a83
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-07-07 16:54:09 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+
+bookList=[
+  {"name":"A Game of Thrones"},
+  {"name":"A Game of Thrones"},
+  {"name":"A Game of Thrones"},
+  {"name":"A Game of Thrones"},
+]
+
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let bookObj = []
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(res=>res.json())
+    .then( obj =>{
+      //console.log(obj)
+      bookObj = obj
+      console.log (bookObj)
+      renderBooks(bookObj)
+  })
+  //return bookObj
 }
 
 function renderBooks(books) {
@@ -12,6 +31,14 @@ function renderBooks(books) {
   });
 }
 
+function processData(){
+  bookL= object
+  console.log(bookL)
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+
+  //renderBooks(bookL)
 });
